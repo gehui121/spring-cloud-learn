2d3676b1db7a7d7d06e000fabcfa936e9ded4b2c
gehui121 <gehui121@163.com>
2018-12-03 21:28:46 +0800

    完善ribbon配置

---
+++ colud-itwolf-parent/itwolf-order-service-impl/src/main/java/com/itwolf/config/RibbonRuleConfig.java
@@ -0,0 +1,20 @@
+package com.itwolf.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Administrator on 2018/12/3 10:12.
+ * 这个配置为全局配置负载均衡策略方式，
+ * 可以单独针对某一个资源通过注解设置其特有的策略或者使用配置文件 <client name>.ribbon.*
+ * @RibbonClient
+ **/
+@Configuration
+public class RibbonRuleConfig {
+
+    //测试完注释掉，使用默认的轮训机制
+//    @Bean
+//    public IRule ribbonRule(){
+//        //全局设置负载均衡策略为随机策略
+//        return new RandomRule();
+//    }
+}
